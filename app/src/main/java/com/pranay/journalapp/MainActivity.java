4656472ce38f5d062c908135cb3759b6f1dfccd1
pranay <pranay4saini@gmail.com>
2022-12-21 00:31:25 +0530

    created signUp activity Layout

--- app/src/main/java/com/pranay/journalapp/MainActivity.java
+++ app/src/main/java/com/pranay/journalapp/MainActivity.java
@@ -2,13 +2,31 @@ package com.pranay.journalapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button logInButton;
+    TextView register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        logInButton = findViewById(R.id.btnLogin);
+        register = findViewById(R.id.tvRegisterHere);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
