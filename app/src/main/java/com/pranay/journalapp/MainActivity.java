305b9cff545d36c31a06981fa56da0b2cbb4901e
pranay <pranay4saini@gmail.com>
2022-12-21 00:52:18 +0530

    Adding firebase Authentication

--- app/src/main/java/com/pranay/journalapp/MainActivity.java
+++ app/src/main/java/com/pranay/journalapp/MainActivity.java
@@ -6,7 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import org.w3c.dom.Text;
 
@@ -14,12 +19,21 @@ public class MainActivity extends AppCompatActivity {
 
     Button logInButton;
     TextView register;
+    EditText emailET,passET;
+    //Firebase Authentication
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser currentUser;
+    //FireStore
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         logInButton = findViewById(R.id.btnLogin);
         register = findViewById(R.id.tvRegisterHere);
+        emailET = findViewById(R.id.etLoginEmail);
+        passET = findViewById(R.id.etLoginPass);
 
         register.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/pranay/journalapp/RegisterActivity.java
+++ app/src/main/java/com/pranay/journalapp/RegisterActivity.java
@@ -1,14 +1,74 @@
 package com.pranay.journalapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class RegisterActivity extends AppCompatActivity {
+    EditText email_create,password_create;
+    Button registerButton;
+
+    //Firebase Authentication
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser currentUser;
+
+    //FireStore
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference collectionReference = db.collection("Users");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        email_create = findViewById(R.id.etRegEmail);
+        password_create = findViewById(R.id.etRegPass);
+        registerButton = findViewById(R.id.btnRegister);
+
+        //Authentication
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                currentUser = firebaseAuth.getCurrentUser();
+
+                if(currentUser !=null){
+                    //user already have account
+
+                }else{
+                    //User does not have account yet
+
+                }
+            }
+        };
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!TextUtils.isEmpty(email_create.getText().toString())
+                && !TextUtils.isEmpty(password_create.getText().toString())){
+                    String email = email_create.getText().toString().trim();
+                    String password = password_create.getText().toString().trim();
+
+                    CreateUserEmailAccount(email,password);
+                }else{
+                    Toast.makeText(RegisterActivity.this
+                            ,"Empty fields!!!",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void CreateUserEmailAccount(String email, String password) {
     }
 }
